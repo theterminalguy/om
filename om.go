eb7318748620c95d02c294a68839921e8f9d7078
theterminalguy <damiansimonpeter@gmail.com>
2021-10-06 20:37:30 -0400

    Define a KeyValue type for holding pairs

--- om.go
+++ om.go
@@ -1,5 +1,14 @@
 package om
+
+type KeyValue struct {
+	Key   string
+	Value interface{}
+}
 type Map struct {
+}
+
+// Adds a key value pair to the map
+func (*Map) Add(kv *KeyValue) {
 
 }
 
@@ -29,13 +38,25 @@ func (*Map) RValues() []interface{} {
 }
 
 // Compares two map for equality
-func (*Map) EQ(m *Map) bool  {
+func (*Map) EQ(m *Map) bool {
 	return false
 }
 
 // Iterates through the map,
 // Yielding each key and value to the callback function.
-// 
-// KeyValue pair are yielded in the order in which they where added 
+//
+// KeyValue pair are yielded in the order in which they where added
 func (*Map) Each(cb func(key string, value interface{})) {
+}
+
+// Iterates through the map,
+// Yielding each key and value to the callback function.
+//
+// KeyValue pair are yielded in reverse order
+func (*Map) REach(cb func(key string, value interface{})) {
+}
+
+// Returns the total number of items in the map
+func (*Map) Size() int {
+	return 0
 }
